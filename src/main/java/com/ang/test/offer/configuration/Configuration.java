8cc932c1e0c24bae02059f64f2ccf54e0d08577f
oscaronte <oscaronte@gmail.com>
2019-09-12 16:46:43 -0500

    hibernate configuration

--- src/main/java/com/ang/test/offer/configuration/Configuration.java
+++ src/main/java/com/ang/test/offer/configuration/Configuration.java
@@ -1,9 +1,12 @@
 package com.ang.test.offer.configuration;
 
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @org.springframework.context.annotation.Configuration
@@ -21,6 +24,23 @@ public class Configuration {
 	    
 	    // Classpath scanning of @Component, @Service, etc annotated class
 	    entityManagerFactory.setPackagesToScan(env.getProperty("entitymanager.packagesToScan"));
+	    
+	    // Vendor adapter
+	    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	    entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
+	    
+	    // Hibernate properties
+	    Properties additionalProperties = new Properties();
+	    additionalProperties.put(
+	        "hibernate.dialect", 
+	        env.getProperty("hibernate.dialect"));
+	    additionalProperties.put(
+	        "hibernate.show_sql", 
+	        env.getProperty("hibernate.show_sql"));
+	    additionalProperties.put(
+	        "hibernate.hbm2ddl.auto", 
+	        env.getProperty("hibernate.hbm2ddl.auto"));
+	    entityManagerFactory.setJpaProperties(additionalProperties);
 	    
 	    return entityManagerFactory;
 	  }
