4103fbb1933e1d6542e388902413dac3189a174c
oscaronte <oscaronte@gmail.com>
2019-09-12 15:23:24 -0500

    package reorganization

--- src/main/java/com/ang/test/offer/repository/impl/OfferRepositoryImpl.java
+++ src/main/java/com/ang/test/offer/repository/impl/OfferRepositoryImpl.java
@@ -1,24 +1,182 @@
 package com.ang.test.offer.repository.impl;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.ang.test.offer.domain.Offer;
 import com.ang.test.offer.repository.OfferRepository;
 
 public class OfferRepositoryImpl implements OfferRepository {
+	@Autowired
+	private EntityManager entityManager;
+	
 	public Offer getOffer(Offer o) {
-		
+		return entityManager.find(Offer.class, o);
 	}
 	
 	public void deleteOffer(Offer o) {
-		
+		entityManager.remove(o);
 	}
 	
 	public void saveOffer(Offer o) {
-		
+		entityManager.persist(o);
 	}
 	
 	public void saveAll(List<Offer> offers) {
+		for(Offer o: offers) {
+			entityManager.persist(o);
+		}
+	}
+
+	@Override
+	public void deleteAllInBatch() {
+		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void deleteInBatch(Iterable<Offer> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Offer> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Offer> findAll(Sort arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Offer> List<S> findAll(Example<S> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Offer> List<S> findAll(Example<S> arg0, Sort arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Offer> findAllById(Iterable<Long> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Offer getOne(Long arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Offer> List<S> saveAll(Iterable<S> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Offer> S saveAndFlush(S arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<Offer> findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Offer> S save(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Offer> findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existsById(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Offer entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll(Iterable<? extends Offer> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <S extends Offer> Optional<S> findOne(Example<S> example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Offer> Page<S> findAll(Example<S> example, Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Offer> long count(Example<S> example) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public <S extends Offer> boolean exists(Example<S> example) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
--- src/main/java/com/ang/test/offer/repository/impl/ProductRepositoryImpl.java
+++ src/main/java/com/ang/test/offer/repository/impl/ProductRepositoryImpl.java
@@ -1,11 +1,16 @@
 package com.ang.test.offer.repository.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.ang.test.offer.domain.Offer;
 import com.ang.test.offer.domain.Product;
@@ -36,7 +41,7 @@ public class ProductRepositoryImpl implements ProductRepository {
 	}
 	
 	public Product getProduct(Product p) {
-		entityManager.find(Product.class, p);
+		return entityManager.find(Product.class, p);
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -44,5 +49,149 @@ public class ProductRepositoryImpl implements ProductRepository {
 		final Query query = entityManager.createQuery("SELECT * FROM Product p WHERE p.name LIKE ?");
 		query.setParameter(1, p.getName());
 		return query.getResultList();
+	}
+
+	@Override
+	public void deleteAllInBatch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteInBatch(Iterable<Product> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Product> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> findAll(Sort arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Product> List<S> findAll(Example<S> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Product> List<S> findAll(Example<S> arg0, Sort arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> findAllById(Iterable<Long> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Product getOne(Long arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Product> List<S> saveAll(Iterable<S> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Product> S saveAndFlush(S arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<Product> findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Product> S save(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Product> findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existsById(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Product entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll(Iterable<? extends Product> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <S extends Product> Optional<S> findOne(Example<S> example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Product> Page<S> findAll(Example<S> example, Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Product> long count(Example<S> example) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public <S extends Product> boolean exists(Example<S> example) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
